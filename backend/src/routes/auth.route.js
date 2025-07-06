082ceebf55406c46fe19826c4491c87249f6235e
snirbennissim <118169229+snirbennissim@users.noreply.github.com>
2025-07-06 20:43:29 +0300

    Project Completed

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,23 @@
+import expreess from "express";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = expreess.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5002/api"
+      : "/api",
+  withCredentials: true,
+});
+
+export default axiosInstance;
